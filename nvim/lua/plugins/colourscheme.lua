4751df1414ad34966aa32cb4dba133605e5eeb77
Markus <46653802+MarkusPriska@users.noreply.github.com>
2024-08-03 23:03:40 +0200

    Got OneDark theme working!

--- nvim/lua/plugins/colourscheme.lua
+++ nvim/lua/plugins/colourscheme.lua
@@ -4,10 +4,36 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
+            -- Initial setup for the onedark theme
             require("onedark").setup({
-                style = "dark",
-        })
-        vim.cmd("colorscheme onedark")
+                style = "dark",  -- or other style you prefer
+            })
+
+            -- Retrieve colors from the theme
+            local c = require("onedark.colors")
+
+            -- Define custom highlights
+            local custom_highlights = {
+                NeoTreeWinSeparator  = { fg = c.none, bg = c.none },
+
+                -- For barbar.nvim
+                BufferOffset         = { bg = c.bg_d },
+
+                -- For neo-tree.nvim
+                -- NeoTreeCursorLine     = { bg = c.bg_d },
+
+            }
+
+            -- Reapply the theme with custom highlights
+            require("onedark").setup({
+                style = "dark",  -- Same style as before
+                highlights = custom_highlights,  -- Apply custom highlights
+            })
+
+            -- Reapply colorscheme to reflect custom highlights
+            vim.cmd("colorscheme onedark")
         end,
-    }
+    },
 }
+
+
--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -8,5 +8,18 @@ return {
         shade_terminals = true,
         direction = "horizontal",
         size = vim.o.lines * 0.3,
+        on_open = function(_)
+            local name = vim.fn.bufname("neo-tree")
+            local winnr = vim.fn.bufwinnr(name)
+
+            if winnr ~= -1 then
+                vim.defer_fn(function()
+                    local cmd = string.format("Neotree toggle")
+                    vim.cmd(cmd)
+                    vim.cmd(cmd)
+                    vim.cmd("wincmd p")
+                end, 100)
+            end
+        end,
     },
 }
